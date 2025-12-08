7a54494a4e69e79124232bd58982de0c3d593079
neominal.solutions <neominal.solutions@gmail.com>
2025-12-08 15:41:45 +0300

    Implement CRUD operations in ProductsController for product management

--- src/main/java/com/akbank/spring_app/controller/ProductsController.java
+++ src/main/java/com/akbank/spring_app/controller/ProductsController.java
@@ -2,9 +2,7 @@ package com.akbank.spring_app.controller;
 
 import com.akbank.spring_app.response.ProductResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -23,4 +21,39 @@ public class ProductsController {
 
         return ResponseEntity.ok(response); // otomatically 200 OK -> Json format
     }
+
+    // @PathVariable -> Path üzerinden dinamik veri almayı sağlar. GET isteklerinde urlden hassas olmayan veriler taşınabilir.
+    // api/v1/products/{id} // api/v1/products/1
+    @GetMapping("{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable String id) {
+        ProductResponse response = new ProductResponse(id,"P-1", BigDecimal.valueOf(10.5),5);
+
+        return ResponseEntity.ok(response); // otomatically 200 OK -> Json format
+    }
+
+    // @RequestBody -> İstek gövdesinden veri almayı sağlar. POST, PUT, DELETE isteklerinde kullanılır.
+    // api/v1/products -> Güvenli Hassas veriler @RequestBody ile taşınır.
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductResponse request) {
+        // DB kaydetme işlemleri yapılır.
+        request.setId(UUID.randomUUID().toString());
+
+        return ResponseEntity.status(201).body(request); // 201 Created
+    }
+
+    // Yanış endoint tanımları -> api/v1/products/update/{id}, api/v1/createProduct // api/v1/product-delete/{id}
+    @PutMapping("{id}") // Resource Update
+    public ResponseEntity<Void> updateProduct(@PathVariable String id, @RequestBody ProductResponse request) {
+        // DB güncelleme işlemleri yapılır.
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{id}") // Resource Delete
+    public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
+        // DB silme işlemleri yapılır.
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
