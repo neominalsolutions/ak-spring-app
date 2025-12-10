4b06caee0d5af37b254cd542306fe2d09e583374
neominal.solutions <neominal.solutions@gmail.com>
2025-12-10 09:20:58 +0300

    Get JsonPropertyName

--- src/main/java/com/akbank/spring_app/error/GlobalErrorHandling.java
+++ src/main/java/com/akbank/spring_app/error/GlobalErrorHandling.java
@@ -1,10 +1,13 @@
 package com.akbank.spring_app.error;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,20 +17,34 @@ public class GlobalErrorHandling {
 
     // MethodArgumentNotValidException böyle bir durum ile karşılaşıldığında fırlatılır.
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,List<Object>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Map<String,List<Object>>> handleValidationExceptions(MethodArgumentNotValidException ex)  {
 
         Map<String, List<Object>> errors = new HashMap<>();
 
         // ["name": ["error1", "error2"], "price": ["error1"]]
-
-        // field name yerine json property name reflect kullanılarak alınabilir miyiz ? 
+        // field name yerine json property name reflect kullanılarak alınabilir miyiz ?
 
         ex.getBindingResult().getFieldErrors().forEach(error -> {
-            errors.putIfAbsent(error.getField(), new java.util.ArrayList<>());
-            errors.get(error.getField()).add(error.getDefaultMessage());
+
+            String jsonFieldName = resolveJsonPropertyName(error, ex.getBindingResult().getTarget().getClass());
+            errors.putIfAbsent(jsonFieldName, new java.util.ArrayList<>());
+            errors.get(jsonFieldName).add(error.getDefaultMessage());
+
         });
 
         return ResponseEntity.status(400).body(errors);
+    }
+
+
+    private static String resolveJsonPropertyName(FieldError error, Class<?> targetClass) {
+        try {
+            Field field = targetClass.getDeclaredField(error.getField());
+            JsonProperty annotation = field.getAnnotation(JsonProperty.class);
+            if (annotation != null && !annotation.value().isBlank()) {
+                return annotation.value();  // JSON’daki isim
+            }
+        } catch (NoSuchFieldException ignored) {}
+        return error.getField(); // annotation yoksa field name
     }
 
 
